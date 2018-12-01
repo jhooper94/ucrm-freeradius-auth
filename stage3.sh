07160fc5bf64836b58e2c22dcb422c0322aa7d70
jhooper94 <43616206+jhooper94@users.noreply.github.com>
2018-12-01 00:14:42 -0600

    Update stage3.sh

--- stage3.sh
+++ stage3.sh
@@ -6,17 +6,17 @@ sudo apt-get install freeradius freeradius-mysql freeradius-utils
 
 mysql -u root -p radius < /etc/freeradius/3.0/mods-config/sql/main/mysql/schema.sql
 
-ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled/
+sudo ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled/
 
 wget https://raw.githubusercontent.com/jhooper94/ucrm-freeradius-auth/master/sql
 
-rm /etc/freeradius/3.0/mods-enabled/sql
+sudo rm /etc/freeradius/3.0/mods-enabled/sql
 
-mv sql /etc/freeradius/3.0/mods-enabled/sql
+sudo mv sql /etc/freeradius/3.0/mods-enabled/sql
 
-chgrp -h freerad /etc/freeradius/3.0/mods-available/sql
+sudo chgrp -h freerad /etc/freeradius/3.0/mods-available/sql
 
-chown -R freerad:freerad /etc/freeradius/3.0/mods-enabled/sql
+sudo chown -R freerad:freerad /etc/freeradius/3.0/mods-enabled/sql
 
 systemctl restart freeradius.service
 
