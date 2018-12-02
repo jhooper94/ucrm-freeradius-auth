814f2b8fe292fdf08fec9be7cab3af797c6a50db
jhooper94 <43616206+jhooper94@users.noreply.github.com>
2018-12-01 20:15:38 -0600

    Update stage2.py

--- stage2.py
+++ stage2.py
@@ -7,9 +7,9 @@ password = raw_input("Type the password you set to log into sql database: ")
 db = MySQLdb.connect("localhost", "root", password)
 cursor = db.cursor()
 try:
-	cursor.execute("create DATABASE radius")
+	cursor.execute('create DATABASE radius')
 	cursor.execute('GRANT ALL ON radius.* TO root@localhost IDENTIFIED BY "radiuspassword"')
-	cursor.execute("FLUSH PRIVILEGES;")
+	cursor.execute('FLUSH PRIVILEGES')
 	db.commit()
 	print "all good"
 	urllib.urlretrieve ("https://raw.githubusercontent.com/jhooper94/ucrm-freeradius-auth/master/stage3.sh", "stage3.sh")
