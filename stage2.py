87eda7f6951f60e676037164b31ec2b1ea07654b
jhooper94 <43616206+jhooper94@users.noreply.github.com>
2018-12-02 14:47:43 -0600

    Update stage2.py

--- stage2.py
+++ stage2.py
@@ -8,15 +8,17 @@ db = MySQLdb.connect("localhost", "root", password)
 cursor = db.cursor()
 try:
 	cursor.execute('create DATABASE radius')
-	cursor.execute('GRANT ALL ON root.* TO root@localhost IDENTIFIED BY "radiuspassword"')
+	cursor.execute('GRANT ALL ON radius.* TO radius@localhost IDENTIFIED BY "radiuspassword"')
 	cursor.execute('FLUSH PRIVILEGES')
 	db.commit()
 	print "all good"
-	urllib.urlretrieve ("https://raw.githubusercontent.com/jhooper94/ucrm-freeradius-auth/master/stage3.sh", "stage3.sh")
-	subprocess.call(['chmod', '+x', 'stage3.sh'])
-	
-	print "Type: sudo su"
-	print "once logged in as root type ./stage3.sh"
 except:
 	db.rollback()
 	print "It didn't work"
+
+	
+urllib.urlretrieve ("https://raw.githubusercontent.com/jhooper94/ucrm-freeradius-auth/master/stage3.sh", "stage3.sh")
+subprocess.call(['chmod', '+x', 'stage3.sh'])
+	
+print "Type: sudo su"
+print "once logged in as root type ./stage3.sh"
