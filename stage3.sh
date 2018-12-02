3d4a3bc60b0333c29830e33589c3b6c48b4c48bf
jhooper94 <43616206+jhooper94@users.noreply.github.com>
2018-12-02 15:45:39 -0600

    Update stage3.sh

--- stage3.sh
+++ stage3.sh
@@ -18,6 +18,26 @@ sudo chown -R freerad:freerad /etc/freeradius/3.0/mods-enabled/sql
 
 systemctl restart freeradius.service
 
+wget https://github.com/lirantal/daloradius/archive/master.zip
+
+unzip master.zip
+
+mv daloradius-master/ daloradius
+
+cd daloradius
+
+mysql -u root -p radius < contrib/db/fr2-mysql-daloradius-and-freeradius.sql 
+
+mysql -u root -p radius < contrib/db/mysql-daloradius.sql
+
+cd ..
+
+sudo mv daloradius /var/www/html/
+
+sudo chown -R www-data:www-data /var/www/html/daloradius/
+
+sudo chmod 664 /var/www/html/daloradius/library/daloradius.conf.php
+
 wget https://raw.githubusercontent.com/jhooper94/ucrm-freeradius-auth/master/ucrmsetup.py
 
 python ucrmsetup.py
